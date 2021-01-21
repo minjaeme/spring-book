ac171af07e4e422f508a914bfb96f43bea9bcd97
minjae <minjae@hyundai-autoever.com>
2021-01-21 16:16:12 +0900

    JUnitTest 도입

--- src/main/java/me/minjae/spring/user/dao/UserDaoTest.java
+++ src/main/java/me/minjae/spring/user/dao/UserDaoTest.java
@@ -1,33 +1,60 @@
 package me.minjae.spring.user.dao;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
 import java.sql.SQLException;
 
+
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import me.minjae.spring.user.domain.User;
 
 public class UserDaoTest {
-
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		// ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+	
+	@Test
+	public void addAndGet() throws ClassNotFoundException, SQLException {
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		UserDao dao = context.getBean("userDao", UserDao.class);
 		
 		User user = new User();
-		user.setId("whiteShip");
-		user.setName("백기선");
-		user.setPassword("married");
-		
+		user.setId("hyumee");
+		user.setName("박성철");
+		user.setPassword("springno1");
+
 		dao.add(user);
 		
-		System.out.println(user.getId() + " 등록 성공");
-		
 		User user2 = dao.get(user.getId());
-		System.out.println(user2.getName());
-		System.out.println(user2.getPassword());
 		
-		System.out.println(user2.getId() + " 조회 성공");
+		assertThat(user2.getName(), is(user.getName()));
+		assertThat(user2.getPassword(), is(user.getPassword()));
+	}
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		
+		JUnitCore.main("me.minjae.spring.user.dao.UserDaoTest");
+//		// ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+//		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+//		UserDao dao = context.getBean("userDao", UserDao.class);
+//		
+//		User user = new User();
+//		user.setId("whiteShip");
+//		user.setName("백기선");
+//		user.setPassword("married");
+//		
+//		dao.add(user);
+//		
+//		System.out.println(user.getId() + " 등록 성공");
+//		
+//		User user2 = dao.get(user.getId());
+//		System.out.println(user2.getName());
+//		System.out.println(user2.getPassword());
+//		
+//		System.out.println(user2.getId() + " 조회 성공");
 	}
 }
